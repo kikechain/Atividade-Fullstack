39f14b9fd818ce770177c27b43f7edca0c113c5b
kikechain <134706891+kikechain@users.noreply.github.com>
2023-05-26 10:36:48 -0300

    Add files via upload

---
+++ Desenvolvimento-Fullstack-API-main/Desenvolvimento Fullstack (API)/serviceWorker.js
@@ -0,0 +1,56 @@
+const cacheName = 'api'
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache){
+            cache.addAll([
+                './',
+                './index.html',
+                './manifest.json',
+                './js/index.js',
+                './js/apple-touch-icon_120.png',
+                './js/apple-touch-icon_180.png',
+                './js/icon_192.png',
+                './js/icon_512.png',
+                './maskable_192.png',
+                './maskable_512.png',
+
+            ])
+        })
+    )
+    return self.skipWaiting()
+})
+
+self.addEventListener('activate', e =>{
+    self.clients.claim()
+})
+
+self.addEventListener('fetch', async e =>{
+    const req = e.request
+    const url = new URL(req.url)
+
+    if(url.origin === location.origin){
+        e.respondWith(cacheFirst(req))
+    } else{
+        e.respondWith(networkAndCache(req))
+    }
+})
+
+async function cacheFirst(req){
+    const cache = await caches.open(cacheName)
+    const cached = await cache.match(req)
+
+    return cached || fetch(req)
+}
+
+async function networkAndCache(req){
+    const cache = await caches.open(cacheName);
+    try{
+        const refresh = await fetch(req)
+        await cache.put(req, fresh.clone())
+        return refresh
+    } catch(e){
+        const cached = await cache.match(req);
+        return cached
+    }
+}
